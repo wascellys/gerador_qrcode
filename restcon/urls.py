19f509329b665d85ae331528e1114714c7091fa7
wascellys <edywascellys@gmail.com>
2021-12-21 09:30:47 -0300

    correcao

--- restcon/urls.py
+++ restcon/urls.py
@@ -5,7 +5,9 @@ from cardapiovirtual import settings
 from restcon.views import gerador_qrcode, gerador_qrcode_pdf, url_cardapio
 
 urlpatterns = [
+    # path('', home, name='home'),
+    # path('produtos/<int:pk>', produtos, name='produtos'),
     path('gerador_qrcode', gerador_qrcode, name='gerador_qrcode'),
+    path('gerador_qrcode_pdf', gerador_qrcode_pdf, name='gerador_qrcode_pdf'),
+    path('url_cardapio', url_cardapio, name='url_cardapio'),
 ]
-
-
